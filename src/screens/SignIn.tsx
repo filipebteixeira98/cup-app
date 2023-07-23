45943a83d2e562f9a9079b823c290e93174aad84
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-07-23 00:33:02 -0300

    chore: created sign in screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,23 @@
-import { Center, Text } from 'native-base';
+import { Center, Icon, Text } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+
+import { Button } from '../components/Button';
+
+import Logo from '../assets/logo.svg';
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        Open up App.js to start working on your app!
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        type="SECONDARY"
+        title="Sign In with Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+      <Text color="white" textAlign="center" mt={4}>
+        We do not use any information other than {'\n'}
+        your e-mail to create your account.
       </Text>
     </Center>
   );
